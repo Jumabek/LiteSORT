645dd3b439f6fe77bfe7e5aff308d365ffb2a737
Jumabek <jumabek4044@gmail.com>
2023-11-09 15:23:12 +0900

    add results folder via git lfs
    Former-commit-id: 3b28c3e8c1f69f73eb8db66316d9f0319cd1d780 [formerly 8ed223019e7838bb8d7222e0299351c5296d56f4]
    Former-commit-id: 1731a9daa8e6d285133ebda8601d2e86664adba6

---
+++ results/detectorParams/run_experiment_repeat.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+
+# Hardcoded Variables
+EXPERIMENT_NAME="detectorParams"
+DATASET="MOT17"
+
+# Base Command
+BASE_CMD="python strong_sort.py ${DATASET} val"
+
+# Function to run tracker
+run_tracker() {
+
+    TRACKER_NAME=$1
+    echo "-----------------------------------"
+    echo "Running tracker: ${TRACKER_NAME} with Input Resolution: ${INPUT_RESOLUTION} and Confidence Threshold: ${MIN_CONFIDENCE}"  # Debug message
+
+    DIR_SAVE="results/${EXPERIMENT_NAME}/${TRACKER_NAME}__input_${INPUT_RESOLUTION}__conf_${MIN_CONFIDENCE}"
+    if [ ! -d "${DIR_SAVE}" ]; then
+        mkdir -p ${DIR_SAVE}
+    fi
+
+    case ${TRACKER_NAME} in
+        "SORT")
+            ${BASE_CMD} --iou_only --dir_save ${DIR_SAVE} --input_resolution ${INPUT_RESOLUTION} --min_confidence ${MIN_CONFIDENCE}
+            ;;
+        "LiteSORT")
+            ${BASE_CMD} --yolosort --dir_save ${DIR_SAVE} --input_resolution ${INPUT_RESOLUTION} --min_confidence ${MIN_CONFIDENCE}
+            ;;
+        "DeepSORT")
+            ${BASE_CMD} --dir_save ${DIR_SAVE} --input_resolution ${INPUT_RESOLUTION} --min_confidence ${MIN_CONFIDENCE}
+            ;;
+        "StrongSORT")
+            ${BASE_CMD} --dir_save ${DIR_SAVE} --input_resolution ${INPUT_RESOLUTION} --min_confidence ${MIN_CONFIDENCE} --BoT --ECC --NSA --EMA --MC --woC
+            ;;
+        *)
+            echo "Invalid tracker name"
+            exit 1
+            ;;
+    esac
+    echo "Experiment completed for ${TRACKER_NAME}!"
+
+    
+
+}
+
+# Iterate over multiple resolutions and confidence thresholds
+for INPUT_RESOLUTION in 320 640 960 1280 1600 1920 2240 2560; do
+    for MIN_CONFIDENCE in .1 .15 .2; do
+        # Run experiments for all trackers
+        #run_tracker "SORT"
+        #run_tracker "LiteSORT"
+        #run_tracker "deepSORT"
+        run_tracker "StrongSORT"
+    done
+done
---
+++ results/off-the-shelf/eval.sh
@@ -0,0 +1,13 @@
+python TrackEval/scripts/run_mot_challenge.py \
+--BENCHMARK MOT17 \
+--SPLIT_TO_EVAL train \
+--TRACKERS_TO_EVAL /home/juma/code/StrongSORT/results/off-the-shelf/StrongSORT__input_1280__conf_.25 \
+--TRACKER_SUB_FOLDER '' \
+--METRICS HOTA CLEAR  Identity VACE  \
+--USE_PARALLEL False \
+--NUM_PARALLEL_CORES 1 \
+--GT_LOC_FORMAT '{gt_folder}/{seq}/gt/gt.txt' \
+--OUTPUT_SUMMARY True \
+--OUTPUT_EMPTY_CLASSES False \
+--OUTPUT_DETAILED False \
+--PLOT_CURVES False
