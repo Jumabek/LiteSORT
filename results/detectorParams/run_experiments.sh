ef5b830e727a79642f1ea1ec2838a0f09aa691c6
Jumabek <jumabek4044@gmail.com>
2023-11-11 09:48:57 +0900

    reensure txt and png to be lfs
    Former-commit-id: 188062577045c7492bdbf90fdd226db91b7b5dda [formerly fc01063a318022fdc80db94e4a57654d3334243b]
    Former-commit-id: 4ed1375027d1d8e50f68f923fccc749422680275

--- results/detectorParams/run_experiments.sh
+++ results/detectorParams/run_experiments.sh
@@ -15,7 +15,7 @@ run_tracker() {
     echo "-----------------------------------"
     echo "Running tracker: ${TRACKER_NAME} with Input Resolution: ${INPUT_RESOLUTION} and Confidence Threshold: ${MIN_CONFIDENCE}"  # Debug message
 
-    DIR_SAVE="results/${EXPERIMENT_NAME}/${TRACKER_NAME}__input_${INPUT_RESOLUTION}__conf_${MIN_CONFIDENCE}"
+    DIR_SAVE="results/${EXPERIMENT_NAME}/fps_runs/${TRACKER_NAME}__input_${INPUT_RESOLUTION}__conf_${MIN_CONFIDENCE}"
     if [ ! -d "${DIR_SAVE}" ]; then
         mkdir -p ${DIR_SAVE}
     fi
@@ -43,7 +43,7 @@ run_tracker() {
 
 # Iterate over multiple resolutions and confidence thresholds
 for INPUT_RESOLUTION in 320 640 960 1280 1600 1920 2240 2560; do
-    for MIN_CONFIDENCE in .25 .3 .35 .4 .45 .5 .55 .6 .65 .7; do
+    for MIN_CONFIDENCE in 0.05 .1 .15 .2 .25 .3 .35 .4 .45 .5 .55 .6 .65 .7; do
         # Run experiments for all trackers
         run_tracker "SORT"
         run_tracker "LiteSORT"
